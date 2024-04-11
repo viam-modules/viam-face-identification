769cf573e14db6704f7756ecc90cf9e412971b18
Rob1in <73953872+Rob1in@users.noreply.github.com>
2024-04-11 11:18:47 -0400

    better README

--- src/face_detection_module.py
+++ src/face_detection_module.py
@@ -16,63 +16,72 @@ from .identifier import Identifier
 from .utils import decode_image
 from PIL import Image
 
-EXTRACTORS = [
-  'mediapipe:0',
-  'mediapipe:1', 
-  'yunet'
-]
+EXTRACTORS = ["mediapipe:0", "mediapipe:1", "yunet"]
 
-ENCODERS = [
-  "sface", 
-  "facenet"
-]
+ENCODERS = ["sface", "facenet"]
 
 LOGGER = getLogger(__name__)
 
+
 class FaceIdentificationModule(Vision, Reconfigurable):
     MODEL: ClassVar[Model] = Model(ModelFamily("viam", "vision"), "face-identification")
-     
+
     def __init__(self, name: str):
         super().__init__(name=name)
-        
+
     @classmethod
-    def new_service(cls,
-                 config: ServiceConfig,
-                 dependencies: Mapping[ResourceName, ResourceBase]) -> Self:
+    def new_service(
+        cls, config: ServiceConfig, dependencies: Mapping[ResourceName, ResourceBase]
+    ) -> Self:
         service = cls(config.name)
         service.reconfigure(config, dependencies)
         return service
-    
-     # Validates JSON Configuration
+
+    # Validates JSON Configuration
     @classmethod
     def validate_config(cls, config: ServiceConfig) -> Sequence[str]:
-        detection_framework = config.attributes.fields["extractor_model"].string_value or 'yunet'
+        detection_framework = (
+            config.attributes.fields["extractor_model"].string_value or "yunet"
+        )
         if detection_framework not in EXTRACTORS:
-            raise Exception("face_extractor_model must be one of: '" + "', '".join(EXTRACTORS) + "'.")
-        model_name = config.attributes.fields["face_embedding_model"].string_value or 'facenet'
+            raise Exception(
+                "face_extractor_model must be one of: '"
+                + "', '".join(EXTRACTORS)
+                + "'."
+            )
+        model_name = (
+            config.attributes.fields["face_embedding_model"].string_value or "facenet"
+        )
         if model_name not in ENCODERS:
-            raise Exception("face embedding model (encoder) must be one of: '" + "', '".join(ENCODERS) + "'.")
+            raise Exception(
+                "face embedding model (encoder) must be one of: '"
+                + "', '".join(ENCODERS)
+                + "'."
+            )
         camera_name = config.attributes.fields["camera_name"].string_value
         if camera_name == "":
             raise Exception(
-                "A camera name is required for face_identification vision service module.")
+                "A camera name is required for face_identification vision service module."
+            )
         return [camera_name]
 
-    def reconfigure(self,
-            config: ServiceConfig,
-            dependencies: Mapping[ResourceName, ResourceBase]):
-        
+    def reconfigure(
+        self, config: ServiceConfig, dependencies: Mapping[ResourceName, ResourceBase]
+    ):
         self.camera_name = config.attributes.fields["camera_name"].string_value
-        self.camera =  dependencies[Camera.get_resource_name(self.camera_name)]
-        def get_attribute_from_config(attribute_name:str,  default, of_type=None):
+        self.camera = dependencies[Camera.get_resource_name(self.camera_name)]
+
+        def get_attribute_from_config(attribute_name: str, default, of_type=None):
             if attribute_name not in config.attributes.fields:
                 return default
 
             if default is None:
                 if of_type is None:
-                    raise Exception("If default value is None, of_type argument can't be empty")
+                    raise Exception(
+                        "If default value is None, of_type argument can't be empty"
+                    )
                 type_default = of_type
-            else:    
+            else:
                 type_default = type(default)
 
             if type_default == bool:
@@ -85,62 +94,92 @@ class FaceIdentificationModule(Vision, Reconfigurable):
                 return config.attributes.fields[attribute_name].string_value
             elif type_default == dict:
                 return dict(config.attributes.fields[attribute_name].struct_value)
-        
-        detector_backend = get_attribute_from_config('extractor_model', 'yunet')
-        extraction_threshold = get_attribute_from_config('extractor_confidence_threshold', .6)
-        grayscale = get_attribute_from_config('grayscale', False)    
-        enforce_detection = get_attribute_from_config('always_run_face_recognition', False)
-        align = get_attribute_from_config('align', True)
-        model_name = get_attribute_from_config('face_embedding_model', 'facenet')
-        normalization = get_attribute_from_config('normalization', 'base')
-        picture_directory = config.attributes.fields['picture_directory'].string_value 
-        distance_metric_name = get_attribute_from_config('distance_metric', 'cosine')
-        identification_threshold = get_attribute_from_config('identification_threshold', None, float)
-        sigmoid_steepness = get_attribute_from_config('sigmoid_steepness',10.)
-        self.identifier = Identifier(detector_backend=detector_backend, 
-                        extraction_threshold=extraction_threshold, 
-                        grayscale=grayscale, 
-                        enforce_detection = enforce_detection, 
-                        align = align,
-                        model_name = model_name,
-                        normalization= normalization,
-                        picture_directory=picture_directory,
-                        distance_metric_name = distance_metric_name,
-                        identification_threshold=identification_threshold,
-                        sigmoid_steepness = sigmoid_steepness,
-                        debug = False)
 
+        detector_backend = get_attribute_from_config("extractor_model", "yunet")
+        extraction_threshold = get_attribute_from_config(
+            "extractor_confidence_threshold", 0.6
+        )
+        grayscale = get_attribute_from_config("grayscale", False)
+        enforce_detection = get_attribute_from_config(
+            "always_run_face_recognition", False
+        )
+        align = get_attribute_from_config("align", True)
+        model_name = get_attribute_from_config("face_embedding_model", "facenet")
+        normalization = get_attribute_from_config("normalization", "base")
+        picture_directory = config.attributes.fields["picture_directory"].string_value
+        distance_metric_name = get_attribute_from_config("distance_metric", "cosine")
+        identification_threshold = get_attribute_from_config(
+            "identification_threshold", None, float
+        )
+        sigmoid_steepness = get_attribute_from_config("sigmoid_steepness", 10.0)
+        self.identifier = Identifier(
+            detector_backend=detector_backend,
+            extraction_threshold=extraction_threshold,
+            grayscale=grayscale,
+            enforce_detection=enforce_detection,
+            align=align,
+            model_name=model_name,
+            normalization=normalization,
+            picture_directory=picture_directory,
+            distance_metric_name=distance_metric_name,
+            identification_threshold=identification_threshold,
+            sigmoid_steepness=sigmoid_steepness,
+            debug=False,
+        )
 
         self.identifier.compute_known_embeddings()
         LOGGER.info(f" Found {len(self.identifier.known_embeddings)} labelled groups.")
-        
-    async def get_object_point_clouds(self, camera_name: str, *, extra: Optional[Dict[str, Any]] = None, timeout: Optional[float] = None, **kwargs) -> List[PointCloudObject]:
+
+    async def get_object_point_clouds(
+        self,
+        camera_name: str,
+        *,
+        extra: Optional[Dict[str, Any]] = None,
+        timeout: Optional[float] = None,
+        **kwargs,
+    ) -> List[PointCloudObject]:
         raise NotImplementedError
-    
-    async def get_detections(self, image: Union[Image.Image, RawImage], *, extra: Mapping[str, Any], timeout: float) -> List[Detection]:
+
+    async def get_detections(
+        self,
+        image: Union[Image.Image, RawImage],
+        *,
+        extra: Mapping[str, Any],
+        timeout: float,
+    ) -> List[Detection]:
         img = decode_image(image)
         return self.identifier.get_detections(img)
-    
-    async def get_classifications(self, image: Union[Image.Image, RawImage], count: int, *, extra: Mapping[str, Any]) -> List[Classification]:
+
+    async def get_classifications(
+        self,
+        image: Union[Image.Image, RawImage],
+        count: int,
+        *,
+        extra: Mapping[str, Any],
+    ) -> List[Classification]:
         return NotImplementedError
-    
+
     async def get_classifications_from_camera(self) -> List[Classification]:
         return NotImplementedError
-    
-    async def get_detections_from_camera(self, camera_name: str, *, extra: Mapping[str, Any],  timeout: float) -> List[Detection]:
+
+    async def get_detections_from_camera(
+        self, camera_name: str, *, extra: Mapping[str, Any], timeout: float
+    ) -> List[Detection]:
         im = await self.camera.get_image(mime_type=CameraMimeType.JPEG)
         img = decode_image(im)
         return self.identifier.get_detections(img)
-    
-    
-    async def do_command(self,
-                        command: Mapping[str, ValueTypes],
-                        *,
-                        timeout: Optional[float] = None,
-                        **kwargs):
-        if command["command"] == "recompute_embeddings" :
+
+    async def do_command(
+        self,
+        command: Mapping[str, ValueTypes],
+        *,
+        timeout: Optional[float] = None,
+        **kwargs,
+    ):
+        if command["command"] == "recompute_embeddings":
             self.identifier.known_embeddings = dict()
             self.identifier.compute_known_embeddings()
             LOGGER.info("Embeddings recomputed!")
             return {"result": "Embeddings recomputed!"}
-        else: raise NotImplementedError
+        else:
+            raise NotImplementedError
