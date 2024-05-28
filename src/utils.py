664439099aabd65342d19962bd04b67b947d26c3
Rob1in <73953872+Rob1in@users.noreply.github.com>
2024-05-28 16:01:15 -0400

    also remove RawImage

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,19 @@
 from typing import Union
-from viam.media.video import ViamImage, RawImage
+from viam.media.video import ViamImage
 from PIL import Image
 from viam.logging import getLogger
 from viam.media.video import CameraMimeType
 import numpy as np
-LOGGER = getLogger(__name__)
-SUPPORTED_IMAGE_TYPE = [CameraMimeType.JPEG,
-                        CameraMimeType.PNG,
-                        CameraMimeType.VIAM_RGBA]
 
-def decode_image(image: Union[Image.Image, RawImage, ViamImage])-> np.ndarray:
+LOGGER = getLogger(__name__)
+SUPPORTED_IMAGE_TYPE = [
+    CameraMimeType.JPEG,
+    CameraMimeType.PNG,
+    CameraMimeType.VIAM_RGBA,
+]
+
+
+def decode_image(image: Union[Image.Image, ViamImage]) -> np.ndarray:
     """decode image to BGR numpy array
 
     Args:
@@ -18,24 +22,30 @@ def decode_image(image: Union[Image.Image, RawImage, ViamImage])-> np.ndarray:
     Returns:
         np.ndarray: BGR numpy array
     """
-    if isinstance(image, (RawImage, ViamImage)):
+    if isinstance(image, ViamImage):
         if image.mime_type not in SUPPORTED_IMAGE_TYPE:
-            LOGGER.error(f"Unsupported image type: {image.mime_type}. Supported types are {SUPPORTED_IMAGE_TYPE}.")
+            LOGGER.error(
+                f"Unsupported image type: {image.mime_type}. Supported types are {SUPPORTED_IMAGE_TYPE}."
+            )
             raise ValueError(f"Unsupported image type: {image.mime_type}.")
-        im = Image.open(image.data).convert('RGB') #convert in RGB png openened in RGBA
+        im = Image.open(image.data).convert(
+            "RGB"
+        )  # convert in RGB png openened in RGBA
         return np.array(im)
-    
-    res = image.convert('RGB')
+
+    res = image.convert("RGB")
     rgb = np.array(res)
     return rgb
     # bgr = rgb[...,::-1]
     # return bgr
 
+
 def dist_to_conf_sigmoid(dist, steep=10):
-    return 1 / (1 + np.exp((steep*(dist-0.5))))
+    return 1 / (1 + np.exp((steep * (dist - 0.5))))
+
 
 def check_ir(img, acc=10):
     h, w = img.shape[0], img.shape[1]
-    r = img[h//2:h//2+h//acc, w//2:w//2+w//acc,0] 
-    g = img[h//2:h//2+h//acc, w//2:w//2+w//acc,1]
-    return (r==g).all()
+    r = img[h // 2 : h // 2 + h // acc, w // 2 : w // 2 + w // acc, 0]
+    g = img[h // 2 : h // 2 + h // acc, w // 2 : w // 2 + w // acc, 1]
+    return (r == g).all()
