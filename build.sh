7ba7237f0afdbe6ad56e682b4ed2ae9c0b331380
Rob1in <73953872+Rob1in@users.noreply.github.com>
2024-07-11 14:07:14 -0400

    remove tar

--- build.sh
+++ build.sh
@@ -31,4 +31,3 @@ echo installing dependencies from requirements.txt
 $VIRTUAL_ENV/bin/pip install -r requirements.txt
 source .venv/bin/activate 
 python3 -m PyInstaller --onefile --hidden-import="googleapiclient" src/main.py
-tar -czvf dist/archive.tar.gz dist/main
