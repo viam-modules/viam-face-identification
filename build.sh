506dc2d4c8738bb006443dcbb9f21c0ab23b984f
Matt Vella <mcvella@gmail.com>
2024-10-24 09:21:15 -0400

    Use uv to easily use the required version of python

--- build.sh
+++ build.sh
@@ -8,26 +8,13 @@ if ! command -v $SUDO; then
 	SUDO=""
 fi
 
-if command -v apt-get; then
-	if dpkg -l python3-venv; then
-		echo "python3-venv is installed, skipping setup"
-	else
-		if ! apt info python3-venv; then
-			echo python3-venv package info not found, trying apt update
-			$SUDO apt-get -qq update
-		fi
-		$SUDO apt-get install -qqy python3-venv
-	fi
-else
-	echo Skipping tool installation because your platform is missing apt-get.
-	echo If you see failures below, install the equivalent of python3-venv for your system.
+if ! command -v uv 2>&1 >/dev/null; then
+    pip install uv
 fi
 
 source .env
-echo creating virtualenv at $VIRTUAL_ENV
-python3 -m venv $VIRTUAL_ENV
-echo installing dependencies from requirements.txt
-$VIRTUAL_ENV/bin/pip install -r requirements.txt
-source .venv/bin/activate 
+uv venv --python 3.11
+source .venv/bin/activate
+uv pip install -r requirements.txt
 $PYTHON -m PyInstaller --onefile --hidden-import="googleapiclient" --add-data "./src/models/checkpoints:checkpoints"  main.py
 tar -czvf dist/archive.tar.gz dist/main
